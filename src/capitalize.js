00fcb347dd91a17e737e1685263180fd9792bb54
mshcherbina <marinashcherbina89@gmail.com>
2023-01-18 21:45:36 +0300

    Change tests with assert

--- src/capitalize.js
+++ src/capitalize.js
@@ -1,6 +1,5 @@
+/* eslint-disable import/prefer-default-export */
 export const capitalize = (text) => {
   const [firstSymbol = '', ...restSymbols] = text;
   return `${firstSymbol.toUpperCase()}${restSymbols.join('')}`;
 };
-
-
